a28ad7869d8815b7067064eedc3dc374743fb514
Igor Buzykin <lexemz@gmail.com>
2022-09-20 21:37:09 +0300

    LayerAndAnimationProject

---
+++ LayerAndAnimation/AnimationsTest/MyViewController.swift
@@ -0,0 +1,60 @@
+//
+//  MyViewController.swift
+//  AnimationsTest
+//
+//  Created by Igor Buzykin on 19.09.2022.
+//
+
+import UIKit
+
+class MyViewController: UIViewController {
+    
+    private var customView: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        recreateView()
+    }
+    
+    @IBAction func recreateViewTap(_ sender: UIBarButtonItem) {
+        recreateView()
+    }
+    
+    // Не смотря на то, что мы разрешаем анимацию - она не произойдет
+    @IBAction func transactionTap(_ sender: UIButton) {
+        CATransaction.begin()
+        CATransaction.setAnimationDuration(1) // default value = 0.25 (1 sec)
+        CATransaction.setDisableActions(false) // Разрешение анимации
+        customView.layer.cornerRadius = customView.layer.frame.width / 2
+        CATransaction.commit()
+    }
+    
+    // Есть анимация
+    @IBAction func nonDelegate(_ sender: UIButton) {
+        customView.layer.delegate = nil
+        customView.layer.cornerRadius = customView.layer.frame.width / 2
+    }
+    
+    private func recreateView() {
+        if let customView = customView {
+            customView.removeFromSuperview()
+            self.customView = nil
+        }
+        
+        customView = UIView()
+        customView.frame.size = .init(width: 100, height: 100)
+        customView.frame.origin = CGPoint(
+            x: view.frame.width / 2 - customView.frame.width / 2,
+            y: view.frame.height / 2 - customView.frame.height / 2
+        )
+        customView.backgroundColor = UIColor.black
+        view.addSubview(customView)
+    }
+}
+
+class LXView: UIView {
+    override func action(for layer: CALayer, forKey event: String) -> CAAction? {
+        print("event: \(event)")
+        return nil
+    }
+}
