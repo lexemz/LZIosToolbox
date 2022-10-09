a4157f2d430a2f52d6c20cd1ea7a779fec0312ef
Igor Buzykin <lexemz@gmail.com>
2022-10-09 17:42:02 +0300

    Create socket.io project

---
+++ Socket.io/Socket.IO/Socket.IO/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Socket.IO
+//
+//  Created by Igor Buzykin on 09.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
