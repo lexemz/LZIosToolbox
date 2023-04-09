ea96ecdd91fe761c82dcbefeda5abef81ade36a2
Igor Buzykin <lexemz@gmail.com>
2023-04-09 22:01:22 +0300

    TwoShapes

--- Drafts/BezierPath/BezierPath/Figures/TwoShapes.swift
+++ Drafts/BezierPath/BezierPath/Figures/TwoShapes.swift
@@ -26,13 +26,14 @@ final class TwoShapes: UIView {
 		createShapeLayer()
 	}
 
+	// Вызвать, когда фрейм вью извествен
 	func createShapeLayer() {
 		let halfX = frame.size.width / 2
 		let halfY = frame.size.height / 2
 
 		let path1 = UIBezierPath()
 		path1.move(to: .zero)
-		path1.addLine(to: CGPoint(x: halfX, y: .zero))
+		path1.addLine(to: CGPoint(x: halfX, y: 0))
 		path1.addLine(to: CGPoint(x: halfX / 2, y: halfY))
 		path1.close()
 		let shapeLayer1 = CAShapeLayer()
@@ -41,14 +42,18 @@ final class TwoShapes: UIView {
 		shapeLayer1.strokeColor = UIColor.red.cgColor
 
 		let path2 = UIBezierPath()
-		path2.move(to: CGPoint(x: halfX / 2, y: .zero))
+		path2.move(to: CGPoint(x: halfX / 2, y: 0))
 		path2.addLine(to: CGPoint(x: halfX, y: halfY))
-		path2.addLine(to: CGPoint(x: .zero, y: halfY))
+		path2.addLine(to: CGPoint(x: 0, y: halfY))
 		path2.close()
 		let shapeLayer2 = CAShapeLayer()
 		shapeLayer2.path = path2.cgPath
 		shapeLayer2.fillColor = UIColor.cyan.cgColor
 		shapeLayer2.strokeColor = UIColor.yellow.cgColor
+
+		// Контроль позиции
+		shapeLayer1.position = CGPoint(x: halfX / 2, y: halfY)
+		shapeLayer2.position = CGPoint(x: halfX / 2, y: 0)
 
 		[shapeLayer1, shapeLayer2].forEach {
 			shapeLayers.append($0)
